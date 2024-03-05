d2f015b5b2b1b3c35c12c7e31d264ecb3e3244ba
Nguyễn Ngọc Tín <131370934+ngoctinn@users.noreply.github.com>
2024-03-05 18:23:21 +0700

    Add project files.

---
+++ Minimart_Project_WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Minimart_Project_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
