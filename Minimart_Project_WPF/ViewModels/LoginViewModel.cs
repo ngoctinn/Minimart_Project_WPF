9a16d5d4b92eb20d4df23263355451b7d1260ca2
Nguyễn Ngọc Tín <131370934+ngoctinn@users.noreply.github.com>
2024-03-06 19:49:13 +0700

    Update MainWindow

---
+++ Minimart_Project_WPF/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Minimart_Project_WPF.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public LoginViewModel()
+        {
+            LoginWindow login = new LoginWindow();
+            login.ShowDialog();
+        }
+    }
+}
--- Minimart_Project_WPF/ViewModels/MainViewModel.cs
+++ Minimart_Project_WPF/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Minimart_Project_WPF.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,7 @@ namespace Minimart_Project_WPF.ViewModels
     {
         public MainViewModel()
         {
-           
+               
         }
     }
 }
