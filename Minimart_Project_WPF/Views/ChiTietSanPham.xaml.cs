528c6cc7a390798d8ade07e5d84295b0aa296d15
ngoctinn <3121410041@sv.sgu.edu.vn>
2024-05-06 00:11:09 +0700

    done add sanpham

--- Minimart_Project_WPF/Views/ChiTietSanPham.xaml.cs
+++ Minimart_Project_WPF/Views/ChiTietSanPham.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using Minimart_Project_WPF.ViewModels;
+using Minimart_Project_WPF.XuLyKhac;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +22,16 @@ namespace Minimart_Project_WPF.Views
     /// </summary>
     public partial class ChiTietSanPham : Window
     {
+        private TaoMaTuDong taoMaTuDong = new TaoMaTuDong();
         public ChiTietSanPham()
         {
             InitializeComponent();
-            WindowStartupLocation = WindowStartupLocation.CenterScreen; // Set the startup location of the window to the center of the screen
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+            // Set the startup location of the window to the center of the screen
+            taoMaTuDong = new TaoMaTuDong();
+            var viewModel = (SanPhamViewModel)this.DataContext;
+            viewModel.MaSanPham = taoMaTuDong.TaoMaTuDongSanPham();
+            txtMaSanPham.Text = viewModel.MaSanPham;
         }
 
         private void btnChonAnh_Click(object sender, RoutedEventArgs e)
---
+++ Minimart_Project_WPF/XuLyKhac/TaoMaTuDong.cs
@@ -0,0 +1,27 @@
+﻿using Minimart_Project_WPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minimart_Project_WPF.XuLyKhac
+{
+    public class TaoMaTuDong
+    {
+        public TaoMaTuDong()
+        {
+            //Tạo mã tự động
+        }
+
+         
+
+        public string TaoMaTuDongSanPham()
+        {
+            using var context = new csassignmentContext();
+            int soSanPham = context.SanPhams.Count();
+            string maSanPham = string.Format("SPH{0:D4}", ++soSanPham);
+            return maSanPham;
+        }
+    }
+}
